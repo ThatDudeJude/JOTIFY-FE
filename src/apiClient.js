e7729e24ee6e208ea8b69948253ee8c1215deb7a
ThatDudeJude <judegachoki@gmail.com>
2023-08-09 17:42:48 +0300

    Corrected apiClient

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import 'dotenv/config';
 
-const apiClient = axios.create({ baseURL: process.env.BASE_URL });
+
+const apiClient = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
 export default apiClient;

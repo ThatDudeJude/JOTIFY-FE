a17ae21cadbc573e7dce4aec36d6c70181826675
ThatDudeJude <judegachoki@gmail.com>
2024-04-25 14:29:17 +0300

    Push for update

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 
-const apiClient = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
+const apiClient = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
 export default apiClient;

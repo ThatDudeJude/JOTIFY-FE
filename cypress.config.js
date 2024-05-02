a34239fe7a15b6fe37b3f2eeed827e0ef0fcf92f
ThatDudeJude <judegachoki@gmail.com>
2024-05-02 16:00:23 +0300

    Reset baseUrl in cypres.config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl: "http://localhost:3000",
-    baseUrl: `${process.env.REACT_APP_API_BASE_URL}`, 
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
